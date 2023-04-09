108f5c5c66f142125a8f95324082529d4d7aa723
Fernando Berti <bertilxi@gmail.com>
2023-04-09 14:31:16 -0300

    feat: new implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ["./node_modules/ares/eslint-config/recommended.js"],
-  // rules: {
-  //   "@typescript-eslint/no-unsafe-member-access": "off",
-  //   "@typescript-eslint/no-unsafe-argument": "off",
-  // },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,46 @@
+type Constructor<T = any> = new (...rest: any[]) => T;
+
+type Getter<T> = () => T;
+
+type TypeOf<Type> = Type extends { type: infer T }
+  ? T extends string
+    ? T
+    : never
+  : never;
+
+type BaseOf<Type> = Type extends { base: infer T }
+  ? T extends Constructor
+    ? T
+    : never
+  : never;
+
 const errors = {
   noInstance: () => new Error("Invalid resolver"),
 };
 
-type AsyncResolver<Instance> = (instance: Instance) => Promise<boolean>;
+class Hemera<
+  Type extends string,
+  Instance,
+  TypeGetter extends [Type, Getter<Instance>]
+> {
+  private resolvers: Map<Type, Getter<Instance>>;
 
-export default class Hemera<Enum extends string, Instance> {
-  public instances: Map<Enum, Instance>;
-
-  constructor(...resolvers: [Enum, Instance][]) {
-    this.instances = new Map<Enum, Instance>();
-
-    for (const [type, resolver] of new Map(resolvers)) {
-      this.instances.set(type, resolver);
-    }
+  constructor(...resolvers: TypeGetter[]) {
+    this.resolvers = new Map(resolvers);
   }
 
-  public get(type: Enum): Instance {
-    const instance = this.instances.get(type);
+  public get(type: Type) {
+    const getter = this.resolvers.get(type);
 
-    if (!instance) {
+    if (!getter) {
       throw errors.noInstance();
     }
 
-    return instance;
+    return getter();
   }
 
-  public async resolve(resolver: AsyncResolver<Instance>): Promise<Instance> {
-    for (const [type] of this.instances) {
+  public async resolve(resolver: (instance: Instance) => Promise<boolean>) {
+    for (const [type] of this.resolvers) {
       const instance = this.get(type);
 
       const result = await resolver(instance);
@@ -38,4 +52,18 @@ export default class Hemera<Enum extends string, Instance> {
 
     throw errors.noInstance();
   }
+}
+
+export default function hemera<
+  Class extends Constructor<InstanceType<Class>> & { type: string; base: any },
+  Type extends TypeOf<Class>,
+  Base extends BaseOf<Class>,
+  TypeGetter extends [Type, Getter<InstanceType<Base>>]
+>(...resolvers: Class[]) {
+  const mapped = resolvers.map((clazz) => [
+    clazz.type,
+    () => new clazz() as Getter<InstanceType<Base>>,
+  ]) as TypeGetter[];
+
+  return new Hemera<Type, InstanceType<Base>, TypeGetter>(...mapped);
 }
